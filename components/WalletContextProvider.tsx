f53b6e9746247a8543961372a4b9b326b308c14b
mateopps <m.perez@viajaseguro.com>
2022-12-20 12:38:24 -0500

    Brave Wallet

--- components/WalletContextProvider.tsx
+++ components/WalletContextProvider.tsx
@@ -5,17 +5,18 @@ import {
 } from "@solana/wallet-adapter-react"
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui"
 import { clusterApiUrl } from "@solana/web3.js"
-import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets"
+import { PhantomWalletAdapter, BraveWalletAdapter } from "@solana/wallet-adapter-wallets"
 import { useMemo } from "react"
 require("@solana/wallet-adapter-react-ui/styles.css")
 
 const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
   const url = useMemo(() => clusterApiUrl("devnet"), [])
   const phantom = new PhantomWalletAdapter()
+  const brave = new BraveWalletAdapter()
 
   return (
     <ConnectionProvider endpoint={url}>
-      <WalletProvider wallets={[phantom]}>
+      <WalletProvider wallets={[phantom,brave]}>
         <WalletModalProvider>{children}</WalletModalProvider>
       </WalletProvider>
     </ConnectionProvider>
